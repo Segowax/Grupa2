4143afaaf1e708610be1770e45ee06c3d5701442
Uczen <Uczen@DESKTOP-FE0UD96>
2024-10-04 11:02:38 +0200

    baza danych

---
+++ WebApp/WebApp.Client/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class Item{
+    public void nazwa() { }
+    public int Id { get; set; }
+
+    public Guid Guid { get; set; }
+
+    public string Name { get; set; }
+
+    public int Quantity { get; set; }
+ }
+
