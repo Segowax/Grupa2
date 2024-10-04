d1dc22708396eb7a2e3e9a645a8fea99492e99e6
Uczen <Uczen@DESKTOP-FE0UD96>
2024-10-04 11:01:11 +0200

    clasa

---
+++ WebApp/WebApp.Client/item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class item
+{
+    public item() { }
+    public int Id { get; set; }
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; } 
+}
+
+      
+    
