18c4159e0bbc985adfe0dea906a301203859b368
Mariia Levchenko <158235106+Yurievna27@users.noreply.github.com>
2024-10-04 20:44:08 +0200

    Item.cs added

--- WebApp/WebApp.Database/Item.cs
+++ WebApp/WebApp.Database/Item.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Item
-{
-	public Item()
-	{
-		int Id;
-		Guid Guid;
-		string Name;
-		int Quantity;
-	}
-}
+namespace WebApp.Database;
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
