290548feeff195cfcfd6c1f31862d31c267d1996
Uczen <Uczen@DESKTOP-FE0UD96>
2024-10-11 12:22:42 +0200

    upd

---
+++ WebApp/ConsoleAppML/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Reflection.Metadata;
+
+var a = new User();
+a.name = "Mary";
+a.surname = "Idk";
+a.password = "password";
+Console.WriteLine($"Name: \"{a.name}\", Surname: \"{a.surname}\", Password: \"{a.password}\"");
+
+var b = new User();
+b.name = a.name;
+b.surname = "Nwm";
+b.password = "passwordddddd";
+Console.WriteLine($"Name: \"{b.name}\", Surname: \"{b.surname}\", Password: \"{b.password}\"");
+
+public class User
+{
+    public string name { get; set; }
+    public string surname { get; set; }
+    public string password { get; set; }
+}
+
