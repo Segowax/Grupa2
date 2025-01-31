5a09e4bf20dc5082a91129defb032c24bc7828bc
HXSXKU <HXSXKU@gmail.com>
2025-01-31 10:28:18 +0100

    Data_base

---
+++ WebApp/WebApp.Domain/Local.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Domain
+{
+    public class Local
+    {
+        public int ID { get; set; }
+        public string Nazwa { get; set; }
+        public string Miasto { get; set; }
+        public string Ulica { get;set; }
+        public int Numer { get; set; }
+    }
+}
---
+++ WebApp/WebApp.Domain/Rezerwacje.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.Domain
+{
+    public class Rezerwacje
+    {
+        public int ID { get; set; }
+        public int nr_stolika { get; set; }
+        public DateTime data_rez { get; set; }
+        public int liczba_osob {  get; set; }
+        public TextWriter telefon {  get; set; } 
+    }
+}
