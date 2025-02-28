0b38dae1aa47e918847565a5ca30c723bce9986c
uczen <uczen@HENRYK>
2025-02-28 12:16:27 +0100

    Arteeeeem

--- WebApp/WebApp.Client/Program.cs
+++ WebApp/WebApp.Client/Program.cs
@@ -6,6 +6,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("sensor" ,client =>
+{
+    client.BaseAddress = new Uri("https://my-wonderful-api-acgvdvaaa5d4bez.northeurope-01.azurewebsites.net");
+});
+builder.Services.AddHttpClient("myApi", client =>
+{
+    client.BaseAddress = new Uri("http://localhost:5062/");
+});
 
 await builder.Build().RunAsync();
