d3417db9bf603646567b2e92d1acffb854274570
uczen <uczen@IGOR>
2025-03-07 12:21:21 +0100

    MAsza

--- WebApp/WebApp.Client/Program.cs
+++ WebApp/WebApp.Client/Program.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp.Client;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args); 
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("Masza-alkasza", client =>
+    {
+        client.BaseAddress = new Uri("https://my-wonderful-api-acgvdvaaa5d4b9ez.northeurope-01.azurewebsites.net");
+    }
+);
 
 await builder.Build().RunAsync();
